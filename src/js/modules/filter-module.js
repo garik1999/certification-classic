6af8db0391aa520fe2eeff2e109d1b43ff6f6774
Игорь Петров <igorpetrov@MacBook-Pro-Igor.local>
2020-11-09 13:16:24 +0200

    Fix render

--- src/js/modules/filter-module.js
+++ src/js/modules/filter-module.js
@@ -47,21 +47,22 @@ export const filter = () => {
 	    $(container).empty();
 	    let count_temp = 0;
 	    for (let key in json){
+			if (json.hasOwnProperty(key)) {
+				console.log(json.hasOwnProperty(key))
+				if(count_temp < count) {
+					let clone = card_temp.content.cloneNode(true);
 
-	      if(count_temp < count) {
-	        let clone = card_temp.content.cloneNode(true);
-
-	        $(clone).attr('data-card', json[key].["id"]);
-	        $(clone).find('.card__brand').text(json[key].["brand"].["name"]);
-	        $(clone).find('.card_img').attr('src', json[key].["image"].["sizes"].["card-preview"]);
-	        $(clone).find('.card__manufacturer').text(json[key].["manufacturer"].["name"]);
-	        $(clone).find('.card__year').text(Math.floor(Math.random() * (2021 - 1973 + 1) ) + 1973);
-	        $(clone).find('.card__model').text(json[key].["model"].["name"]);
-	        $(clone).find('.card__price').text(json[key].["price"].["currency"].["symbol"]+json[key].["price"].["value"]);
-
-	        container.append(clone);
-	        count_temp++;
-	      }
+					$(clone).attr('data-card', json[key]["id"]);
+					$(clone).find('.card__brand').text(json[key]["brand"]["name"]);
+					$(clone).find('.card_img').attr('src', json[key]["image"]["sizes"]["card-preview"]);
+					$(clone).find('.card__manufacturer').text(json[key]["manufacturer"]["name"]);
+					$(clone).find('.card__year').text(Math.floor(Math.random() * (2021 - 1973 + 1) ) + 1973);
+					$(clone).find('.card__model').text(json[key]["model"]["name"]);
+					$(clone).find('.card__price').text(json[key]["price"]["currency"]["symbol"]+json[key]["price"]["value"]);
+					container.append(clone);
+					count_temp++;
+				}
+			}
 	    }
 	  }
 
